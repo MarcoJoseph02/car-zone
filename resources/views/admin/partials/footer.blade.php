635349309eff934d424219b230cdd4a08545f604
MarcoJoseph02 <marojojo707@gmail.com>
2025-06-13 01:27:03 +0300

    add images and update text in admin dashboard

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -3,7 +3,7 @@
         @if(LaravelLocalization::getCurrentLocale() === 'en')
 
             made with <i class="text-danger fa fa-heart"></i> by
-            <a href="" class="font-weight-bold" target="_blank">{{__('app.app name') }}</a>
+            <a href="https://science.asu.edu.eg/ar/" class="font-weight-bold" target="_blank">{{__('Faculty of Science students') }}</a>
             ©
             <script>
                 document.write(new Date().getFullYear())
